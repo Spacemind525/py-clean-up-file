8819f302559fcaf50a0501455c800a2818390912
Alex <zigzaksasha@gmail.com>
2025-07-07 10:11:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ class CleanUpFile:
     def __enter__(self) -> str:
         return self
 
-    def __exit__(self, exc_type: type, exc_val: BaseException, exc_tb: object) -> None:
+    def __exit__(self,
+                 exc_type: type,
+                 exc_val: BaseException,
+                 exc_tb: object) -> None:
         try:
             if self.filename:
                 os.remove(self.filename)
